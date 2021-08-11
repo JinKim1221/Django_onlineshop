10962b6d05ca74d623d60855e7ecc9598b9752f4
JinKim1221 <sdo7329@gmail.com>
2021-08-11 16:38:01 +0100

    add social login ex. google, naver

--- config/settings.py
+++ config/settings.py
@@ -41,6 +41,12 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'shop',
+    'django.contrib.sites',
+    'allauth',
+    'allauth.account',
+    'allauth.socialaccount',
+    'allauth.socialaccount.providers.naver',
+    'allauth.socialaccount.providers.google',
 ]
 
 MIDDLEWARE = [
@@ -134,3 +140,12 @@ STATIC_URL = '/static/'
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+AUTHENTICATION_BACKENDS = [
+    'django.contrib.auth.backends.ModelBackend',
+    'allauth.account.auth_backends.AuthenticationBackend',
+]
+
+SITE_ID = 1
+
+LOGIN_REDIRECT_URL = '/'
--- shop/views.py
+++ shop/views.py
@@ -10,11 +10,15 @@ def product_in_category(request, category_slug=None):
     products = Product.objects.filter(available_display=True)
 
     if category_slug:
-        current_category = get_object_or_404(Category, slug=category_slug)
+        current_category = get_object_or_404(Category, slug = category_slug)
         products = products.filter(category=current_category)
 
     return render(request, 'shop/list.html',
-                  {'current_category': current_category, 'categories': categories, 'products': products})
+                  {
+                      'current_category': current_category, 
+                      'categories': categories, 
+                      'products': products
+                  })
 
 def product_detail(request, id, product_slug=None):
     product = get_object_or_404(Product, id=id, slug=product_slug)
