220d9b41dccb398ac39eeb5ca53ebb79a9dd3fcd
JinKim1221 <sdo7329@gmail.com>
2021-08-18 15:30:52 +0100

    created cart view

--- cart/views.py
+++ cart/views.py
@@ -1,3 +1,21 @@
-from django.shortcuts import render
-
+from django.shortcuts import render, redirect, get_object_or_404
+from django.views.decorators.http import require_POST
 # Create your views here.
+
+from shop.models import Product
+from .froms import AddProductForm
+from .cart import Cart
+
+def add(request, product_id):
+    cart = Cart(request)
+    product = get_object_or_404(Product, id=product_id)
+
+    # data is delivered from client to server 
+    # sql injection should be checked
+    form = AddProductForm(request.POST)
+
+    if form.is_vaild():
+        cd = form.cleaned_data
+        cart.add(product=product, quantity=cd['quantity'], is_update=cd['is_update'])
+
+    return redirect('cart:detail')
