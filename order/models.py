58e8223d7509f25bfe959a40de256073ea633521
JinKim1221 <sdo7329@gmail.com>
2021-09-05 23:00:57 +0100

    created Order transaction manager

--- order/models.py
+++ order/models.py
@@ -44,6 +44,40 @@ class OrderItem(models.Model):
     def __str__(self):
         return str(self.id)
     
-    def get_price_item(self) :
+    def get_price_item(self):
         return self.price * self.quantity
+
+import hashlib
+from  .iamport import payments_prepare, find_transaction
+
+class OrderTransactionManager(models.Manager):
+    def create_new(self, order, amount, success=None, transaction_status=None):
+        if not order:
+            raise ValueError("Error in Order")
+
+        order_hash = hashlib.sha1(str(order.id).encode('utf-8')).hexdigest()
+        email_hash = str(order.email).split("@")[0]
+        final_hash = hashlib.sha1((order_hash+email_hash).encode('utf-8')).hexdigest()[:10]
+        merchant_order_id = str(final_hash)
+        payments_prepare(merchant_order_id, amount)
+
+        transaction = self.model(
+            order = order,
+            merchant_order_id = merchant_order_id,
+            amount = amount,
+        )
+
+        if success is not None:
+            transaction.success = success
+            transaction.transaction_status = transaction_status
+
+        try:
+            transaction.save()
+        except Exception as e:
+            print("Error in Save", e)
+
         
+        return transaction.merchant_order_id
+
+class OrderTransaction(models.Model):
+    pass
