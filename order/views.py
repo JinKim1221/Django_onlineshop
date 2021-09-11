9f9df55e84bcd244d4dc3856be5f95489a65d2c5
JinKim1221 <sdo7329@gmail.com>
2021-09-11 20:47:19 +0100

    created order_create with ajax

--- order/views.py
+++ order/views.py
@@ -32,3 +32,28 @@ def order_complete(request):
     # order = Order.objects.get(id=order_id)
     order = get_object_or_404(Order, id=order_id)
     return render(request, 'order/created.html', {'order':order})
+
+from django.views.generic.base import View
+from django.http import JsonResponse
+class order_create_AjaxView():
+    def post(self, request, *args, **kwargs):
+        if not request.user.is_authenticated:
+            return JsonResponse({"authenticated":False}, status=403)
+        
+        cart = Cart(request)
+        form = OrderCreateForm(request.POST)
+        if form.is_valid():
+            order = form.save(commit=False)
+            if cart.coupon:
+                order.coupon = cart.coupon
+                order.discount = cart.get_discount_total()
+            order.save()
+            for item in cart:
+                OrderItem.objects.create(order=order, product=item['product'], price=item['price'], quantity=item['quantity'])
+            cart.clear()
+            data = {
+                "order_id" : order.id
+            }
+            return JsonResponse(data)
+        else:
+            return JsonResponse({}, status=401)
