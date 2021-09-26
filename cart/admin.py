2914bce27585841ebbfe20787576f861b6e2db58
JinKim1221 <sdo7329@gmail.com>
2021-09-26 19:20:59 +0100

    created detail and pdf page link

--- cart/admin.py
+++ cart/admin.py
@@ -30,9 +30,23 @@ class OrderItemInline(admin.TabularInline):
     model = OrderItem
     raw_id_fields = ['product']
 
+from django.urls import reverse
+from django.utils.safestring import mark_safe
+
+def order_detail(obj):
+    url = reverse('orders:admin_order_detail', args=[obj.id])
+    html = mark_safe(f"<a href='{url}'>Detail</a>")
+    return html
+order_detail.short_description = 'Detail' # HEAD in admin page 
+
+def order_pdf(obj):
+    url = reverse('orders:admin_order_pdf', args=[obj.id])
+    html = mark_safe(f"<a href='{url}'>PDF</a>")
+    return html
+order_pdf.short_description = 'PDF'
 
 class OrderAdmin(admin.ModelAdmin):
-    list_display = ['id', 'first_name', 'last_name', 'email', 'address', 'postal_code', 'city', 'paid', 'created', 'updated']
+    list_display = ['id', 'first_name', 'last_name', 'email', 'address', 'postal_code', 'city', 'paid', order_detail, order_pdf, 'created', 'updated']
     list_filter = ['paid', 'created', 'updated']
     inlines = [OrderItemInline]
     actions = [export_to_csv]
