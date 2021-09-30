1d16f6fa62f7fbe0e27ac58bd30a263cdff042c5
JinKim1221 <sdo7329@gmail.com>
2021-09-30 23:37:43 +0100

    created order pdf.html

--- order/views.py
+++ order/views.py
@@ -138,7 +138,7 @@ import weasyprint
 @staff_member_required
 def admin_order_pdf(request, order_id):
     order = get_object_or_404(Order, id=order_id)
-    html = render_to_string('order/admin.pdf.html', {'order':order})
+    html = render_to_string('order/admin/pdf.html', {'order':order})
     response = HttpResponse(content_type='application/pdf')
     response['Content-Disposition'] = f'filename=order_{order.id}.pdf'
     weasyprint.HTML(string=html).write_pdf(response, stylesheet=[weasyprint.CSS(settings.STATICFILES_DIR[0]+'/css/pdf.css')])
